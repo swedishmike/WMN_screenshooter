4a7bf07a7ada815e39bef4efd877ba64959db3c2
Mike Eriksson <swedishmike@users.noreply.github.com>
2020-11-28 10:49:32 +0000

    Removed lots of gunk from the unthreaded version too.

--- WMN_screenshooter.py
+++ WMN_screenshooter.py
@@ -3,7 +3,7 @@
 """
     Author : Micah Hoffman (@WebBreacher)
     Additions by: Mike Eriksson (@swedishmike)
-    Description : Takes each username from the web_accounts_list.json file and performs the lookup to see if the entry is still valid and tries to take a screenshot of the valid ones.
+    Description : Takes each username from the web_accounts_list.json file and performs the lookup to see if the entry is still valid and tries to take a screenshot of the valid ones. 
 
 """
 import argparse
@@ -147,13 +147,6 @@ def web_call(location):
         return resp
 
 
-def random_string(length):
-    return "".join(
-        random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits)
-        for x in range(length)
-    )
-
-
 def finaloutput():
     if len(overall_results) > 0:
         print("------------")
@@ -188,24 +181,6 @@ except:
     sys.exit(1)
 
 
-if args.site:
-    # cut the list of sites down to only the requested ones
-    args.site = [x.lower() for x in args.site]
-    data["sites"] = [x for x in data["sites"] if x["name"].lower() in args.site]
-    if len(data["sites"]) == 0:
-        print(" -  Sorry, the requested site or sites were not found in the list")
-        sys.exit()
-    sites_not_found = len(args.site) - len(data["sites"])
-    if sites_not_found:
-        print(
-            " -  WARNING: %d requested sites were not found in the list"
-            % sites_not_found
-        )
-    print(" -  Checking %d sites" % len(data["sites"]))
-else:
-    print(" -  %s sites found in file." % len(data["sites"]))
-
-
 for site in data["sites"]:
     code_match, string_match = False, False
     # Examine the current validity of the entry
@@ -227,16 +202,10 @@ for site in data["sites"]:
     # Perform initial lookup
     # Pull the first user from known_accounts and replace the {account} with it
     url_list = []
-    if args.username:
-        url = site["check_uri"].replace("{account}", args.username)
-        url_list.append(url)
-        uname = args.username
-    else:
-        account_list = site["known_accounts"]
-        for each in account_list:
-            url = site["check_uri"].replace("{account}", each)
-            url_list.append(url)
-            uname = each
+    url = site["check_uri"].replace("{account}", args.username)
+    url_list.append(url)
+    uname = args.username
+
     for each in url_list:
         print(" -  Looking up %s" % each)
         r = web_call(each)
@@ -255,73 +224,6 @@ for site in data["sites"]:
                 all_found_sites.append(each)
             continue
 
-        if code_match and string_match:
-            # print('     [+] Response code and Search Strings match expected.')
-            # Generate a random string to use in place of known_accounts
-            url_fp = site["check_uri"].replace("{account}", random_string(20))
-            r_fp = web_call(url_fp)
-            if isinstance(r_fp, str):
-                # If this is a string then web got an error
-                print(r_fp)
-                continue
-
-            code_match = r_fp.status_code == int(site["account_existence_code"])
-            string_match = r_fp.text.find(site["account_existence_string"]) > 0
-
-            if code_match and string_match:
-                print("      -  Code: %s; String: %s" % (code_match, string_match))
-                print(
-                    bcolors.RED
-                    + "      !  ERROR: FALSE POSITIVE DETECTED. Response code and Search Strings match "
-                    "expected." + bcolors.ENDC
-                )
-                # TODO set site['valid'] = False
-                overall_results[site["name"]] = "False Positive"
-            else:
-                # print('     [+] Passed false positives test.')
-                pass
-        elif code_match and not string_match:
-            # TODO set site['valid'] = False
-            print(
-                bcolors.RED
-                + '      !  ERROR: BAD DETECTION STRING. "%s" was not found on resulting page.'
-                % site["account_existence_string"]
-                + bcolors.ENDC
-            )
-            overall_results[site["name"]] = "Bad detection string."
-            if args.stringerror:
-                file_name = "se-" + site["name"] + "." + uname
-                # Unicode sucks
-                file_name = file_name.encode("ascii", "ignore").decode("ascii")
-                error_file = codecs.open(file_name, "w", "utf-8")
-                error_file.write(r.text)
-                print("Raw data exported to file:" + file_name)
-                error_file.close()
-
-        elif not code_match and string_match:
-            # TODO set site['valid'] = False
-            print(
-                bcolors.RED
-                + "      !  ERROR: BAD DETECTION RESPONSE CODE. HTTP Response code different than "
-                "expected." + bcolors.ENDC
-            )
-            overall_results[
-                site["name"]
-            ] = "Bad detection code. Received Code: %s; Expected Code: %s." % (
-                str(r.status_code),
-                site["account_existence_code"],
-            )
-        else:
-            # TODO set site['valid'] = False
-            print(
-                bcolors.RED
-                + "      !  ERROR: BAD CODE AND STRING. Neither the HTTP response code or detection "
-                "string worked." + bcolors.ENDC
-            )
-            overall_results[site["name"]] = (
-                "Bad detection code and string. Received Code: %s; Expected Code: %s."
-                % (str(r.status_code), site["account_existence_code"])
-            )
 
 if all_found_sites:
     print("Trying to capture screenshot(s) from the identified site(s) now.")
