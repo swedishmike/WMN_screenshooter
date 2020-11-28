f98f15dcc445094bfbcf84aedf655d4708d07b8f
Mike Eriksson <swedishmike@users.noreply.github.com>
2020-11-28 11:50:31 +0000

    README updated and threading seems to be working ok(ish).

--- WMN_screenshooter_threading.py
+++ WMN_screenshooter_threading.py
@@ -3,7 +3,8 @@
 """
     Author : Micah Hoffman (@WebBreacher)
     Additions by: Mike Eriksson (@swedishmike)
-    Description : Takes each username from the web_accounts_list.json file and performs the lookup to see if the entry is still valid and tries to take a screenshot of the valid ones.
+    Description : Takes each username from the web_accounts_list.json file and performs the lookup to see if the entry is still valid and tries to take a screenshot of the valid ones. 
+                  This is the threading version.
 
 """
 import argparse
@@ -24,7 +25,8 @@ from datetime import datetime
 
 import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
 
 ###################
 # Variables && Functions
@@ -126,7 +128,6 @@ else:
 
 def signal_handler(*_):
     print(bcolors.RED + " !!!  You pressed Ctrl+C. Exiting script." + bcolors.ENDC)
-    finaloutput()
     sys.exit(0)
 
 
@@ -134,7 +135,7 @@ def web_call(location):
     try:
         # Make web request for that URL, timeout in X secs and don't verify SSL/TLS certs
         resp = requests.get(
-            location, headers=headers, timeout=60, verify=False, allow_redirects=False
+            location, headers=headers, timeout=60, verify=False, allow_redirects=False, 
         )
     except requests.exceptions.Timeout:
         return (
@@ -154,66 +155,19 @@ def web_call(location):
         return resp
 
 
-def random_string(length):
-    return "".join(
-        random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits)
-        for x in range(length)
-    )
-
-
-def finaloutput():
-    if len(overall_results) > 0:
-        print("------------")
-        print('The following previously "valid" sites had errors:')
-        for site_with_error, results in sorted(overall_results.items()):
-            print(
-                bcolors.YELLOW
-                + "     %s --> %s" % (site_with_error, results)
-                + bcolors.ENDC
-            )
-    else:
-        print(":) No problems with the JSON file were found.")
-
-
-###################
-# Main
-###################
-
-# Add this in case user presses CTRL-C
-signal.signal(signal.SIGINT, signal_handler)
-
-# Suppress HTTPS warnings
-requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-
+def read_in_the_json_file(filelocation):
 # Attempt to read in the JSON file
-try:
-    with open(args.config) as data_file:
-        data = json.load(data_file)
-except:
-    print(bcolors.RED + " Could not find the JSON file", args.config + bcolors.ENDC)
-    print(bcolors.RED + " Exiting...." + bcolors.ENDC)
-    sys.exit(1)
+    try:
+        with open(filelocation) as data_file:
+            data = json.load(data_file)
+    except:
+        print(bcolors.RED + " Could not find the JSON file", args.config + bcolors.ENDC)
+        print(bcolors.RED + " Exiting...." + bcolors.ENDC)
+        sys.exit(1)
 
+    return(data)
 
-if args.site:
-    # cut the list of sites down to only the requested ones
-    args.site = [x.lower() for x in args.site]
-    data["sites"] = [x for x in data["sites"] if x["name"].lower() in args.site]
-    if len(data["sites"]) == 0:
-        print(" -  Sorry, the requested site or sites were not found in the list")
-        sys.exit()
-    sites_not_found = len(args.site) - len(data["sites"])
-    if sites_not_found:
-        print(
-            " -  WARNING: %d requested sites were not found in the list"
-            % sites_not_found
-        )
-    print(" -  Checking %d sites" % len(data["sites"]))
-else:
-    print(" -  %s sites found in file." % len(data["sites"]))
-
-
-for site in data["sites"]:
+def validate_site(site):
     code_match, string_match = False, False
     # Examine the current validity of the entry
     if not site["valid"]:
@@ -222,115 +176,46 @@ for site in data["sites"]:
             + " *  Skipping %s - Marked as not valid." % site["name"]
             + bcolors.ENDC
         )
-        continue
     if not site["known_accounts"][0]:
         print(
             bcolors.CYAN
             + " *  Skipping %s - No valid user names to test." % site["name"]
             + bcolors.ENDC
         )
-        continue
 
-    # Perform initial lookup
-    # Pull the first user from known_accounts and replace the {account} with it
-    url_list = []
+    # # Perform initial lookup
+    # # Pull the first user from known_accounts and replace the {account} with it
+    url = site["check_uri"].replace("{account}", args.username)
+    uname = args.username
+
+    # print(" -  Looking up %s" % url)
+    r = web_call(url)
+    if isinstance(r, str):
+        # We got an error on the web call
+        print(r)
+
+    # Analyze the responses against what they should be
+    code_match = r.status_code == int(site["account_existence_code"])
+    string_match = r.text.find(site["account_existence_string"]) >= 0
+
     if args.username:
-        url = site["check_uri"].replace("{account}", args.username)
-        url_list.append(url)
-        uname = args.username
-    else:
-        account_list = site["known_accounts"]
-        for each in account_list:
-            url = site["check_uri"].replace("{account}", each)
-            url_list.append(url)
-            uname = each
-    for each in url_list:
-        print(" -  Looking up %s" % each)
-        r = web_call(each)
-        if isinstance(r, str):
-            # We got an error on the web call
-            print(r)
-            continue
-
-        # Analyze the responses against what they should be
-        code_match = r.status_code == int(site["account_existence_code"])
-        string_match = r.text.find(site["account_existence_string"]) >= 0
-
-        if args.username:
-            if code_match and string_match:
-                print(bcolors.GREEN + "[+] Found user at %s" % each + bcolors.ENDC)
-                all_found_sites.append(each)
-            continue
-
         if code_match and string_match:
-            # print('     [+] Response code and Search Strings match expected.')
-            # Generate a random string to use in place of known_accounts
-            url_fp = site["check_uri"].replace("{account}", random_string(20))
-            r_fp = web_call(url_fp)
-            if isinstance(r_fp, str):
-                # If this is a string then web got an error
-                print(r_fp)
-                continue
+            print(bcolors.GREEN + "[+] Found user at %s" % url + bcolors.ENDC)
+            all_found_sites.append(url)
+            # continue
 
-            code_match = r_fp.status_code == int(site["account_existence_code"])
-            string_match = r_fp.text.find(site["account_existence_string"]) > 0
+def set_up_threads_sitechecks(data):
+    for site in data['sites']:
+        x = threading.Thread(target=validate_site, args=(site,))
+        threads_sites.append(x)
+    
+    for thread in threads_sites:
+        thread.start()
 
-            if code_match and string_match:
-                print("      -  Code: %s; String: %s" % (code_match, string_match))
-                print(
-                    bcolors.RED
-                    + "      !  ERROR: FALSE POSITIVE DETECTED. Response code and Search Strings match "
-                    "expected." + bcolors.ENDC
-                )
-                # TODO set site['valid'] = False
-                overall_results[site["name"]] = "False Positive"
-            else:
-                # print('     [+] Passed false positives test.')
-                pass
-        elif code_match and not string_match:
-            # TODO set site['valid'] = False
-            print(
-                bcolors.RED
-                + '      !  ERROR: BAD DETECTION STRING. "%s" was not found on resulting page.'
-                % site["account_existence_string"]
-                + bcolors.ENDC
-            )
-            overall_results[site["name"]] = "Bad detection string."
-            if args.stringerror:
-                file_name = "se-" + site["name"] + "." + uname
-                # Unicode sucks
-                file_name = file_name.encode("ascii", "ignore").decode("ascii")
-                error_file = codecs.open(file_name, "w", "utf-8")
-                error_file.write(r.text)
-                print("Raw data exported to file:" + file_name)
-                error_file.close()
+    for thread in threads_sites:
+        thread.join()
 
-        elif not code_match and string_match:
-            # TODO set site['valid'] = False
-            print(
-                bcolors.RED
-                + "      !  ERROR: BAD DETECTION RESPONSE CODE. HTTP Response code different than "
-                "expected." + bcolors.ENDC
-            )
-            overall_results[
-                site["name"]
-            ] = "Bad detection code. Received Code: %s; Expected Code: %s." % (
-                str(r.status_code),
-                site["account_existence_code"],
-            )
-        else:
-            # TODO set site['valid'] = False
-            print(
-                bcolors.RED
-                + "      !  ERROR: BAD CODE AND STRING. Neither the HTTP response code or detection "
-                "string worked." + bcolors.ENDC
-            )
-            overall_results[site["name"]] = (
-                "Bad detection code and string. Received Code: %s; Expected Code: %s."
-                % (str(r.status_code), site["account_existence_code"])
-            )
-
-if all_found_sites:
+def grab_screenshots(all_found_sites):
     print("Trying to capture screenshot(s) from the identified site(s) now.")
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
@@ -342,6 +227,7 @@ if all_found_sites:
     )
 
     try:
+        print(bcolors.GREEN + 'The screenshots will be stored in ', image_directory + bcolors.ENDC)
         os.makedirs(image_directory)
     except OSError as e:
         if e.errno != errno.EEXIST:
@@ -362,3 +248,27 @@ if all_found_sites:
         driver.get_screenshot_as_file(image_directory + "/" + filename)
 
     driver.close()
+
+
+def main():
+    print('Would have started the program now')
+
+    # Add this in case user presses CTRL-C
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Suppress HTTPS warnings
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    
+    # Read in the Json file with site definitions
+    data = read_in_the_json_file(args.config)
+    
+    # Create the threads to check all the sites
+    set_up_threads_sitechecks(data)
+
+    if all_found_sites:
+        grab_screenshots(all_found_sites)
+    else:
+        print('No sites found')
+
+if __name__ == "__main__":
+    main()
